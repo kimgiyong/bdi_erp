a70f204a2e86cf227fcd15a66df4474552ca4e0c
koitt-03-P <koitt-03-P@koitt-03-P-PC>
2018-09-06 20:34:50 +0900

    1

--- src/com/bdi/erp/servlet/UserServlet.java
+++ src/com/bdi/erp/servlet/UserServlet.java
@@ -1,7 +1,6 @@
 package com.bdi.erp.servlet;
 
 import java.io.IOException;
-
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
@@ -9,10 +8,10 @@ import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import com.bdi.erp.service.UserService;
 import com.bdi.erp.service.impl.UserServiceImpl;
 
